995ecca304178776fd885e1f130e0214e8bec2dc
Kevin Xiao <aquarius.c.c.a@gmail.com>
2026-05-16 23:02:48 +0800

    刪除舊資料

--- 第三十四章_script與noscript標籤/demos/js/body-end-main.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const target = document.querySelector("#bodyEndTarget");
-  if (!target) return;
-
-  target.textContent = "body-end-main.js found this DOM element because the script is near the end of body.";
-  window.scriptDemo.log("body-end-main.js executed after its target element existed");
-})();
--- 第三十四章_script與noscript標籤/demos/js/defer-library.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  window.scriptDemo = window.scriptDemo || {};
-  window.scriptDemo.libraryReady = true;
-  window.scriptDemo.log("defer-library.js executed: libraryReady = true");
-})();
